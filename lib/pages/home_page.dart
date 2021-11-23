1220bcecdc561ac028b4e275aaf01b73123a71aa
Vitor Rondon <rondon.vitor@yahoo.com.br>
2021-11-22 21:51:00 -0300

    Adjusted movie list layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'package:movieapp/models/movies_model.dart';
 import 'package:movieapp/repositories/movies_repository.dart';
 import 'package:movieapp/repositories/movies_repository_imp.dart';
 import 'package:movieapp/service/dio_service_imp.dart';
+import 'package:movieapp/utils/apis.utils.dart';
+import 'package:movieapp/widgets/custom_list_card_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,18 +21,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ValueListenableBuilder<Movies?>(
-        valueListenable: _controller.movies,
-        builder: (_, movies, __) {
-          return movies != null
-              ? ListView.builder(
-                  itemCount: movies.listMovies.length,
-                  itemBuilder: (_, idx) => Text(
-                    movies.listMovies[idx].title,
-                  ),
-                )
-              : Container();
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(28),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              Text(
+                'Movies',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              ValueListenableBuilder<Movies?>(
+                valueListenable: _controller.movies,
+                builder: (_, movies, __) {
+                  return movies != null
+                      ? ListView.separated(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: movies.listMovies.length,
+                          itemBuilder: (_, idx) => CustomListCardWidget(
+                              movie: movies.listMovies[idx]),
+                          separatorBuilder: (_, __) => const Divider(),
+                        )
+                      : Container();
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_list_card_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp/models/movies_model.dart';
+import 'package:movieapp/utils/apis.utils.dart';
+
+class CustomListCardWidget extends StatelessWidget {
+  final Movie movie;
+
+  const CustomListCardWidget({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      decoration: const BoxDecoration(color: Colors.black54),
+      child: Row(
+        children: [
+          Image.network(
+            API.REQUEST_IMG(movie.posterPath),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movie.title,
+                    style: Theme.of(context).textTheme.headline6,
+                    softWrap: true,
+                    overflow: TextOverflow.visible,
+                  ),
+                  const Spacer(),
+                  Text('Popularidade: ' + movie.popularity.toString()),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text('Votos: ' + movie.voteAverage.toString()),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
